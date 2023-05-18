72400d0e58e04ee45969dc06b3e8241d7144ea42
Martynas <martezq@gmail.com>
2023-05-18 21:00:05 +0300

    css improvements

--- mysite/profile/adapters.py
+++ mysite/profile/adapters.py
@@ -1,9 +1,18 @@
 from allauth.account.adapter import DefaultAccountAdapter
-from django.core.mail import send_mail
-
+from django.contrib.auth import get_user_model
 
 
 class AccountAdapter(DefaultAccountAdapter):
-    # def send_mail(self, template_prefix, email, context):
-        # Customize the email sending process, e.g., using Django's email sending functions
-    pass
+
+    def confirm_email(self, request, email_address):
+        # Confirm the email using the parent class method
+        super().confirm_email(request, email_address)
+
+        # Get the user associated with the confirmed email
+        User = get_user_model()
+        user = User.objects.get(email=email_address.email)
+
+        # Set email_verified field to True and save the user
+        user.email_verified = True
+        user.save()
+
