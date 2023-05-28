01b635cfcf4df2b01e7fca4d2c1c80f895d7a8a0
Martynas <martezq@gmail.com>
2023-05-28 15:19:45 +0300

    added requirements

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
 
+def should_skip_directory(directory):
+    # Skip hidden directories or virtual environment directories
+    return directory.startswith('.') or directory == 'venv'
+
 def print_directory_tree(start_path):
     for root, dirs, files in os.walk(start_path):
+        # Remove skipped directories from the dirs list (modifies os.walk behavior)
+        dirs[:] = [d for d in dirs if not should_skip_directory(d)]
+
         level = root.replace(start_path, '').count(os.sep)
         indent = ' ' * 4 * (level)
         print(f"{indent}{os.path.basename(root)}/")
